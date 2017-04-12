c7dd4d41e56a021b96b066233593a0f257be8184
yk10850 <yk10850@ly.com>
2017-04-12 11:53:48 +0800

    Filter

---
+++ MVCDemoTwo/Filter/ActionfilterDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemoTwo.Filter
+{
+    public class ActionfilterDemo : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            filterContext.HttpContext.Response.Write("开始时间:" + DateTime.Now.ToString() + "<br/>");
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+            var controllerName = filterContext.RouteData.Values["controller"].ToString();
+            var actionName = filterContext.RouteData.Values["action"].ToString();
+
+            filterContext.HttpContext.Response.Write("结束时间:" + DateTime.Now.ToString() + "<br/>");
+            filterContext.HttpContext.Response.Write("controller:" + controllerName + ",action:" + actionName);
+        }
+    }
+}
